d857eeb5dc7032c545a61ecb3d403ed7a06316ff
jjjjooaaoo <jgabrielmello@hotmail.com>
2026-03-18 22:43:12 -0300

    Atalização backend

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,13 @@ app.use(express.json())
 
 // Conexão com MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB conectado")
-})
-.catch(err => {
-  console.log("ERRO REAL DO MONGO:")
-  console.log(err)
+  .then(() => {
+    console.log("MongoDB conectado")
+  })
+  .catch(err => {
+    console.log("ERRO REAL DO MONGO:")
+    console.log(err)
+  })
 
 // Rotas
 const authRoutes = require("./routes/authRoutes")
@@ -28,12 +29,12 @@ app.use("/consulta", consultaRoutes)
 
 // Rota teste
 app.get("/", (req, res) => {
-    res.send("API da clínica funcionando")
+  res.send("API da clínica funcionando")
 })
 
 // Porta
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
